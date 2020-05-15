071e9c513dfb4bed8d724b8f51e6c7886b782e3e
Luca Venturini <lucventurini@gmail.com>
2020-05-15 16:09:11 +0100

    Added *now* the setup.py

---
+++ setup.py
@@ -0,0 +1,79 @@
+# coding: utf-8
+
+"""Setup file for PyPI"""
+
+from setuptools import setup, find_packages
+# from setuptools.extension import Extension
+# from distutils.extension import Extension
+# from Cython.Build import cythonize
+# from codecs import open
+from os import path
+import glob
+# import re
+import sys
+# import numpy as np
+# from scipy._build_utils import numpy_nodepr_api
+
+
+here = path.abspath(path.dirname("__file__"))
+
+# with open(path.join(here, "DESCRIPTION.md"), encoding="utf-8") as description:
+#     long_description = description.read()
+
+# version = {}
+# with open(path.join(here, "Mikado", "version.py")) as fp:
+#     exec(fp.read(), version)
+# version = version["__version__"]
+
+version = "0.1alpha"
+
+# if version is None:
+#     print("No version found, exiting", file=sys.stderr)
+#     sys.exit(1)
+
+if sys.version_info.major != 3:
+    raise EnvironmentError("""Mikado is a pipeline specifically programmed for python3,
+    and is not compatible with Python2. Please upgrade your python before proceeding!""")
+
+
+setup(
+    name="pytritex",
+    version=version,
+    description="Python3-port of Tritex",
+    # long_description=long_description,
+    url="https://github.com/lucventurini/pytritex",
+    author="Luca Venturini",
+    author_email="lucventurini@gmail.com",
+    license="LGPL3",
+    tests_require=["pytest"],
+    classifiers=[
+        "Development Status :: 1 - Alpha",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
+        "Operating System :: POSIX :: Linux",
+        "Framework :: Pytest",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        'Programming Language :: Python :: 3.7'
+    ],
+    # ext_modules=cythonize(extensions, compiler_directives = {"language_level": "3"}),
+    # zip_safe=False,
+    keywords="wheat genomics",
+    packages=find_packages(),
+    # scripts=glob.glob("util/*.py"),
+    # entry_points={"console_scripts": ["mikado = Mikado:main",
+    #                                   "daijin = Mikado.daijin:main",
+    #                                   ]},
+    install_requires=[line.rstrip() for line in open("requirements.txt", "rt")],
+    # extras_require={
+    #     "postgresql": ["psycopg2"],
+    #     "mysql": ["mysqlclient>=1.3.6"],
+    #     "bam": ["pysam>=0.8"]
+    # },
+    # test_suite="nose2.collector.collector",
+    package_data={
+        "pytritex.share": glob.glob(path.join("pytritex", "share", "*pickle*"))
+        },
+    include_package_data=True
+)
