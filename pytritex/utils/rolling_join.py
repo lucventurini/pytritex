4c384254c4ab80b1f6cd3c197b669c6a239c6623
Luca Venturini <lucventurini@gmail.com>
2020-05-20 23:37:25 +0100

    Modified rolling_join so that hopefully it will be faster.

--- pytritex/utils/rolling_join.py
+++ pytritex/utils/rolling_join.py
@@ -5,20 +5,24 @@ import numpy as np
 def rolling_join(left: pd.DataFrame, right: pd.DataFrame, on, by):
     """Implementation of the R data.table rolling join procedure."""
 
-    _cp_right = right.sort_values([on, by])
-    left = left.sort_values([on, by])
-    groups = left.groupby([on])
-    left.loc[:, "__idx_pos"] = groups[by].transform(lambda s: np.arange(s.shape[0], dtype=np.int))
-    assert "__idx_pos" in left.columns
+    assert on in right.columns
+    assert on in left.columns
+    right = right.sort_values([on, by])
+    left = left.copy().sort_values([on, by])
+    left.loc[:, "__idx_pos"] = left.groupby(on)[by].transform(lambda s: np.arange(s.shape[0], dtype=np.int))
+    left = left.set_index([on])
 
-    for on_key, right_indices in iter(_cp_right.groupby([on]).groups.items()):
-        if on_key in groups.groups.keys():
-            _cp_right.loc[right_indices, "__idx_pos"] = np.maximum(
-                0, np.searchsorted(groups.get_group(on_key)[by], _cp_right.loc[right_indices, by]) - 1)
+    def ssorted(row):
+        if row[on] in left.index:
+            return np.maximum(0, np.searchsorted(left.loc[row[on], by], row[by]) - 1)
         else:
-            _cp_right.loc[right_indices, "__idx_pos"] = np.nan
+            return np.nan
 
-    res = left.drop(by, axis=1).merge(_cp_right, on=[on, "__idx_pos"], how="right").drop("__idx_pos", axis=1)
-    # assert res.loc[res[by].isna()].shape[0] <= right.loc[right[by].isna()].shape[0]
+    assert "__idx_pos" in left.columns
+    assert on in right.columns
+
+    right.loc[:, "__idx_pos"] = right.apply(ssorted, axis=1)
+
+    res = left.reset_index(drop=False).drop(by, axis=1).merge(
+        right, on=[on, "__idx_pos"], how="right").drop("__idx_pos", axis=1)
     return res
-
