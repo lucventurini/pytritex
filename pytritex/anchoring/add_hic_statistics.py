488a2ccb59c4df3cb769b458cc411612b4197cc9
Luca Venturini <lucventurini@gmail.com>
2020-05-20 16:52:09 +0100

    Solved a bug which caused a crash when adding the HiC coverage.

--- pytritex/anchoring/add_hic_statistics.py
+++ pytritex/anchoring/add_hic_statistics.py
@@ -29,20 +29,24 @@ def add_hic_statistics(anchored_css: pd.DataFrame, fpairs: pd.DataFrame):
         columns={"scaffold_index2": "scaffold_index", "chr1": "hic_chr"}
     ).groupby(["scaffold_index", "hic_chr"]).size().to_frame("N").reset_index(drop=False)
     hic_stats.loc[:, "N"] = pd.to_numeric(hic_stats["N"], downcast="unsigned")
+    N_dtype, chr_dtype = hic_stats["N"].dtype, hic_stats["hic_chr"].dtype
     grouped_hic_stats = hic_stats.sort_values(["scaffold_index", "N"], ascending=[True, False])
     hic_stats = grouped_hic_stats.groupby("scaffold_index", sort=False).agg(
         hic_chr=("hic_chr", first), hic_chr2=("hic_chr", second), hic_N1=("N", first), hic_N2=("N", second),
-        Nhic=("N", "sum")).astype({"hic_N1": hic_stats["N"].dtype, "hic_N2": hic_stats["N"].dtype,
-                                   "hic_chr": hic_stats["hic_chr"].dtype, "hic_chr2": hic_stats["hic_chr"].dtype})
-    hic_stats.loc[:, "Nhic"] = pd.to_numeric(hic_stats["Nhic"], downcast="float")
+        Nhic=("N", "sum"))
+    type_mapper = {"Nhic": N_dtype, "hic_N1": N_dtype, "hic_N2": N_dtype,
+                   "hic_chr": chr_dtype, "hic_chr2": chr_dtype}
+    for key in ["Nhic", "hic_N1", "hic_N2", "hic_chr", "hic_chr2"]:
+        if hic_stats[key].isna().any() is True and hic_stats[key].isin([0]).any() is True:
+            continue
+        try:
+            hic_stats.loc[:, key] = hic_stats[key].fillna(0).astype(type_mapper[key])
+        except ValueError:
+            raise ValueError((key, hic_stats[key].value_counts(dropna=False).sort_values().head(20)))
     hic_stats.loc[:, "hic_pchr"] = pd.to_numeric(hic_stats["hic_N1"].div(hic_stats["Nhic"], fill_value=0),
                                                  downcast="float")
     hic_stats.loc[:, "hic_p12"] = pd.to_numeric(hic_stats["hic_N2"].div(hic_stats["hic_N1"], fill_value=0),
                                                 downcast="float")
     anchored_css = hic_stats.merge(anchored_css, right_on="scaffold_index", how="right", left_index=True)
-    anchored_css.loc[:, ["Nhic", "hic_N1", "hic_N2"]] = anchored_css[["Nhic", "hic_N1", "hic_N2"]].fillna(0).astype(
-        {"Nhic": hic_stats["Nhic"].dtype, "hic_N1": hic_stats["hic_N1"].dtype, "hic_N2": hic_stats["hic_N2"].dtype}
-    )
-    # for col in ["Nhic", "hic_N1", "hic_N2"]:
-    #     anchored_css.loc[:, col] = anchored_css[col].fillna(0)
+
     return anchored_css, anchored_hic_links
