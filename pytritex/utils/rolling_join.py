802561087a2f876b4253ef9417a591c4b3c96229
Luca Venturini <lucventurini@gmail.com>
2020-05-21 01:33:47 +0100

    Rewritten rolling-join so that we merge the dataframes prior to any .apply. Hopefully this will speed things up.

--- pytritex/utils/rolling_join.py
+++ pytritex/utils/rolling_join.py
@@ -1,5 +1,21 @@
 import pandas as pd
 import numpy as np
+from functools import partial
+
+
+def _ssorted(row, by, how):
+    res = np.searchsorted(row["by"], row[by], side="left")
+    if how == "right":
+        res -= 1
+        if res < 0:
+            return np.nan
+        else:
+            return res
+    elif how == "left":
+        if res == len(row["by"]):
+            return np.nan
+        else:
+            return res
 
 
 def rolling_join(left: pd.DataFrame, right: pd.DataFrame, on, by, how="right"):
@@ -10,47 +26,15 @@ def rolling_join(left: pd.DataFrame, right: pd.DataFrame, on, by, how="right"):
     # right = right.sort_values([on, by])
     left = left.copy().sort_values([on, by])
     grouped = left.groupby(on)
+    s = grouped[by].agg(by=("by", lambda col: col.values.tolist()))
     left.loc[:, "__idx_pos"] = grouped[by].transform(lambda s: np.arange(s.shape[0], dtype=np.int))
-    hashed = dict((_[0], _[1].values) for _ in iter(grouped[by]))
-
-    def ssorted(row):
-        if row[on] in hashed:
-            l = hashed[row[on]]
-            print(l.shape)
-            if l.shape[0] == 1:
-                if how == "right":
-                    if l[0] > row[by]:
-                        return np.nan
-                    else:
-                        return 0
-                elif how == "left":
-                    if l[0] < row[by]:
-                        return np.nan
-                    else:
-                        return 0
-            else:
-                print(how)
-                res = np.searchsorted(l, row[by], side="left")
-                if how == "right":
-                    res -= 1
-                    if res < 0:
-                        return np.nan
-                    else:
-                        return res
-                elif how == "left":
-                    if res == left.loc[row[on], "__idx_pos"].max():
-                        return np.nan
-                    else:
-                        return res
-        else:
-            return np.nan
-
-    assert "__idx_pos" in left.columns
-    assert on in right.columns
-
-    right.loc[:, "__idx_pos"] = right[[on, by]].apply(ssorted, axis=1)
-
-    res = left.drop(by, axis=1).merge(
-        right, on=[on, "__idx_pos"], how="right").drop("__idx_pos", axis=1)
-    # assert res.loc[res[by].isna()].shape[0] <= right.loc[right[by].isna()].shape[0]
+    merged = s.merge(right, how="outer",
+                     left_index=True, right_on=on).sort_values([on, by])
+    merged = merged[~merged[by].isna()]
+    bait = ~merged["by"].isna()
+    _ss = partial(_ssorted, by=by, how=how)
+    merged.loc[bait, "__idx_pos"] = merged.loc[bait, ["by", by]].apply(_ss, axis=1)
+    res = left.drop(by, axis=1).merge(merged.drop("by", axis=1),
+                                      on=[on, "__idx_pos"], how="right").drop("__idx_pos", axis=1)
+    res = res[~res[by].isna()]
     return res
