fa7613f8201bd499292df0e75303faf8284f50c7
Luca Venturini <lucventurini@gmail.com>
2020-06-01 09:37:02 +0100

    Improved collapse_bins, now completely at C++ level

--- pytritex/sequencing_coverage/collapse_bins.pyx
+++ pytritex/sequencing_coverage/collapse_bins.pyx
@@ -2,7 +2,7 @@ import numpy as np
 cimport numpy as np
 cimport cython
 from libcpp.map cimport map as cpp_map
-# distutils: language = c++
+# distutils: language=c++
 np.import_array()
 
 
@@ -12,20 +12,23 @@ cpdef collapse_bins(np.ndarray bins, long binsize):
     cdef cpp_map[long, long] mapper
     cdef cpp_map[long, long].iterator it
     cdef long pos1, pos2
-    cdef long pos
-    cdef long size
-    cdef long value
-    cdef long[:,:] bin_view = bins.view(long)
-    cdef long row_pos
+    cdef long pos, second_pos
+    cdef int size
+    cdef int value
+    cdef np.ndarray[np.int_t, ndim=2, mode="c"] bin_view = bins
+    cdef int row_pos
+    cdef long binsize_c = binsize
 
-    for row_pos in range(bin_view.size // 2):
-        pos1, pos2 = bin_view[row_pos, :]
-        for pos in range(pos1 + binsize, pos2, binsize):
+    for row_pos in range(0, bin_view.shape[0], 1):
+        pos1 = bin_view[row_pos, 0] + binsize_c
+        pos2 = bin_view[row_pos, 1]
+        while pos < pos2:
             it = mapper.find(pos)
             if it == mapper.end():
                 value = 1
             else:
                 value = mapper[pos] + 1
             mapper[pos] = value
+            pos += binsize_c
 
     return mapper
