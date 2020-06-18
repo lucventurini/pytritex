fedf80a9fe7ae6d3874e92921aa46c9ddee4ba6c
Luca Venturini <lucventurini@gmail.com>
2020-06-18 18:40:11 +0100

    BROKEN. However, now it is possible to have the results running all on a laptop. Need to finish the scaffolding part.

--- pytritex/graph_utils/make_super.py
+++ pytritex/graph_utils/make_super.py
@@ -9,49 +9,6 @@ import dask.dataframe as dd
 from pytritex.graph_utils.make_super_path import make_super_path
 
 
-# make_super<-function(hl, cluster_info, prefix="super", cores=1, paths=T, path_max=0, known_ends=F,
-# 		     maxiter=100, verbose=T){
-#
-#  hl[cluster1 %in% cluster_info[excluded == F]$cluster & cluster2 %in% cluster_info[excluded == F]$cluster]->hl
-#
-#  hl[cluster1 < cluster2]->e
-#  graph.edgelist(as.matrix(e[, .(cluster1, cluster2)]), directed=F)->g
-#  E(g)$weight<-e$weight
-#
-#  data.table(cluster=V(g)$name, super=paste(prefix, sep="_", clusters(g)$membership))->mem
-#  cluster_info[mem, on="cluster"]->mem
-#
-#  mem[, .(super_size=.N, length=.N, chr=unique(na.omit(chr))[1], cM=mean(na.omit(cM))), keyby=super]->info
-#  mem[, .(cluster1=cluster, super)][hl, on="cluster1"]->e
-#
-#  list(super_info=info, membership=mem, graph=g, edges=e)->s
-#
-#  if(paths){
-#   if(path_max > 0){
-#    idx<-head(s$super_info[order(-length)], n=path_max)$super
-#   } else {
-#    idx<-s$super_info$super
-#   }
-#   rbindlist(mclapply(mc.cores=cores, idx, function(i) {
-#    start <- end <- NULL
-#    # Take terminal nodes from genetic map
-#    if(known_ends){
-#     s$mem[super == i & !is.na(cM)][order(cM)]$cluster->x
-#     start=x[1]
-#     end=tail(x,1)
-#    }
-#    make_super_path(s, idx=i, start=start, end=end, maxiter=maxiter, verbose=verbose)->x
-#    if(verbose){
-#     cat(paste0("Chromosome ", head(s$mem[super == i]$chr, n=1), " finished.\n"))
-#    }
-#    x
-#   }))[s$membership, on="cluster"]->s$membership
-#  }
-#
-#  s
-# }
-
-
 def _concatenator(edges, membership, known_ends=False, maxiter=100, verbose=False):
     start = end = None
     if known_ends:
@@ -111,6 +68,7 @@ def make_super(hl: dd.DataFrame, cluster_info: dd.DataFrame,
     membership = pd.DataFrame().assign(cidx=cidx_list, super=ssuper).set_index("cidx")
     membership = membership.merge(cidx.reset_index(drop=False), on=["cidx"], how="outer")
     membership = cluster_info.merge(membership, on="cluster", how="right")
+    membership = membership.persist()
     # info <- mem[, .(super_size=.N, length=.N, chr=unique(na.omit(chr))[1], cM=mean(na.omit(cM))),
     # keyby=super]
     # chr=("chr", lambda s: np.nan if s.dropna().shape[0] == 0 else s.dropna().unique()[0])
@@ -138,8 +96,9 @@ def make_super(hl: dd.DataFrame, cluster_info: dd.DataFrame,
             idx = super_object["super_info"].sort_values("length", ascending=False).head(path_max)["super"].unique()
         else:
             idx = super_object["super_info"]["super"].unique()
-        grouped_edges = super_object["edges"].groupby("super").groups
-        grouped_membership = cms.groupby("super").groups
+        idx = idx.compute()
+        grouped_edges = super_object["edges"].groupby("super")
+        grouped_membership = cms.groupby("super")
         order = super_object["membership"].query("super in @idx")[[
             "super", "chr"]].sort_values(["chr"])
         print(time.ctime(), "Starting super, with", idx.shape[0], "positions to analyse.")
