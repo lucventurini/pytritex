a677a74d8f735e99793ef10550cea05630f0c76a
Luca Venturini <lucventurini@gmail.com>
2020-06-25 15:02:17 +0100

    Bfix for the branch remover.

--- pytritex/graph_utils/branch_remover.py
+++ pytritex/graph_utils/branch_remover.py
@@ -28,13 +28,14 @@ def _initial_branch_remover(client: Client,
         out = make_super_scaffolds(links=links, save_dir=save_dir,
             client=client, membership=membership,
             info=info, excluded=excluded, ncores=ncores)
-        membership = dd.read_parquet(out["membership"], infer_divisions=True)
+        membership = out["membership"]
+        dd_membership = dd.read_parquet(membership, infer_divisions=True)
         # Now we have to exclude from consideration those scaffolds
         # that are the backbone of super-scaffolds where there is at least a scaffold with
         # rank > 1.
         # Ie: remove the backbones so that the branches can be reassigned to somewhere else.
-        a = membership.merge(
-            membership.loc[membership["rank"] > 1, ["super", "bin"]].drop_duplicates(),
+        a = dd_membership.merge(
+            dd_membership.loc[dd_membership["rank"] > 1, ["super", "bin"]].drop_duplicates(),
             how="inner", on=["super", "bin"])
         add = a.loc[a["rank"] == 0, :]
         if add.shape[0].compute() == 0:
