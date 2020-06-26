0981ea10987ed88c097d0167d729ba6967bacf53
Luca Venturini <lucventurini@gmail.com>
2020-06-26 15:32:07 +0100

    Added logging and other niceties to orient_scaffods

--- pytritex/scaffold_10x/orient_scaffolds.py
+++ pytritex/scaffold_10x/orient_scaffolds.py
@@ -5,6 +5,9 @@ import dask.dataframe as dd
 import dask.array as da
 from dask.delayed import delayed
 from dask.distributed import Client
+import logging
+logger = logging.getLogger("distributed.worker")
+import time
 # from .make_agp import make_agp
 
 #  m[super_nbin > 1, .(scaffold1=scaffold, bin1=bin, super1=super)][link_pos, on="scaffold1", nomatch=0]->a
@@ -64,6 +67,7 @@ def orient_scaffolds(info: str, res: str,
     res = dd.read_parquet(res, infer_divisions=True)
 
     # #  m[super_nbin > 1, .(scaffold1=scaffold, bin1=bin, super1=super)][link_pos, on="scaffold1", nomatch=0]->a
+    logger.warning("%s Getting scaffolds in super-scaffolds", time.ctime())
     m_greater_one = membership.query("super_nbin > 1")
     assert m_greater_one.shape[0].compute() > 0, membership.head()
     left = m_greater_one[["bin", "super"]].rename(columns={"bin": "bin1", "super": "super1"})
@@ -78,6 +82,7 @@ def orient_scaffolds(info: str, res: str,
     func = delayed(dd.merge)(left, association, on="scaffold_index2", how="inner")
     association = client.compute(func).result()
     association = association.persist()
+    logger.warning("%s Obtained the association table", time.ctime())
 
     association = association.query("(super1 == super2) & (bin1 != bin2)")
     association = association.eval("d = abs(bin2 - bin1)")
@@ -86,6 +91,7 @@ def orient_scaffolds(info: str, res: str,
     association = association.persist()
     assert association.shape[0].compute() > 0
 
+    logger.warning("%s Grouping the association table by SI", time.ctime())
     grouped = association.groupby("scaffold_index")
     nxt = grouped.apply(
         lambda group: group.loc[group.eval("bin2 > bin1"), "pos1"].mean(), meta=float).to_frame("nxt")
@@ -98,6 +104,7 @@ def orient_scaffolds(info: str, res: str,
     final_association = final_association.persist()
     # assert final_association.shape[0].compute() > 0
     # "x != x" means: return the np.nan indices
+    logger.warning("%s Calculating the orientation variable for scaffolds", time.ctime())
     idx1 = final_association.eval("(prv == prv) & (nxt == nxt)")
     index = final_association.index.compute()
     orientation = pd.Series([np.nan] * index.shape[0], index=index)
@@ -116,10 +123,12 @@ def orient_scaffolds(info: str, res: str,
     # assert "scaffold_index" in membership.columns
     # assert "scaffold_index" == final_association.index.name == membership.index.name, (association.index.name,
     #                                                                     membership.index.name)
+    logger.warning("%s Merging the orientation variable back into membership", time.ctime())
     func = delayed(dd.merge)(client.scatter(membership),
                              client.scatter(final_association[["orientation"]]),
                              left_index=True, right_index=True, how="left")
     membership = client.compute(func).result()
+    logger.warning("%s Merged the orientation variable back into membership", time.ctime())
 
     # Now assign an "orientation" value to each (1 if + or unknown, -1 for -)
     # and an "oriented" value (strictly boolean)
@@ -134,34 +143,50 @@ def orient_scaffolds(info: str, res: str,
     oriented = da.from_array(oriented, chunks=chunks)
     membership["orientation"] = orientation
     membership["oriented"] = oriented
+    logger.warning("%s Calculated the orientation/oriented columns", time.ctime())
     # Next step: assign to each scaffold a base-pair position in the super-scaffold, "super_pos"
     # This position should be determined by bin and rank.
 
+    logger.warning("%s Calculating the positions within super-scaffolds", time.ctime())
     super_pos = membership.groupby("super").apply(super_position, meta=np.int).compute()
     super_pos = np.concatenate(super_pos.values)
     chunks = tuple(membership.map_partitions(len).compute().values.tolist())
     super_pos = da.from_array(super_pos, chunks=chunks)
     membership["super_pos"] = super_pos
+    logger.warning("%s Added the super_pos columns", time.ctime())
 
     # Now we have to assign the genetic positions, ie anchor
     anchored = membership[~membership["chr"].isna()]
+    logger.warning("%s Calculating nchr", time.ctime())
     nchr = anchored.groupby(["chr", "super"])["length"].size().to_frame("nchr").reset_index(drop=False)
     nchr_sum = nchr.groupby("super")["nchr"].transform("sum", meta=np.int)
+    logger.warning("%s Calculating pchr", time.ctime())
     nchr["pchr"] = nchr["nchr"] / nchr_sum.compute()
     nchr["max_nchr"] = nchr.groupby("super")["nchr"].transform("max", meta=np.int).values
+    logger.warning("%s Dropping max_nchr duplicates", time.ctime())
     nchr = nchr.query("nchr == max_nchr").drop_duplicates(subset="super").drop("max_nchr", axis=1)
-    nchr_with_cms = dd.merge(membership[~membership["cM"].isna()],
-                             nchr, on=["chr", "super"])
+    logger.warning("%s Merging into nchr_with_cms", time.ctime())
+    func = delayed(dd.merge)(client.scatter(membership[~membership["cM"].isna()]),
+                             client.scatter(nchr), on=["chr", "super"])
+    nchr_with_cms = client.compute(func).result()
+    logger.warning("%s Merged into nchr_with_cms", time.ctime())
     grouped_nchr_with_cms = nchr_with_cms.groupby("super")
+    logger.warning("%s Calculating the nchr_with_cms stats", time.ctime())
     nchr_with_cms["min_cM"] = grouped_nchr_with_cms["cM"].transform("min")
     nchr_with_cms["max_cM"] = grouped_nchr_with_cms["cM"].transform("max")
     nchr_with_cms["cM"] = grouped_nchr_with_cms["cM"].transform("mean")
-    res = dd.merge(nchr, res, on=["super"])
-    assert "super" in res.columns()
+    logger.warning("%s Calculated the nchr_with_cms stats", time.ctime())
+    func = delayed(dd.merge)(client.scatter(nchr), client.scatter(res), on=["super"])
+    res = client.compute(func).result()
     res = res.persist()
-    res = dd.merge(nchr_with_cms, res, on="super")
+    assert "super" in res.columns
+    func = delayed(dd.merge)(client.scatter(nchr_with_cms),
+                             client.scatter(res), on="super")
+    res = client.compute(func).result()
+    logger.warning("%s Merged everything into res, saving", time.ctime())
     res_name = os.path.join(save_dir, "res")
     dd.to_parquet(res, res_name, compression="gzip", compute=True, engine="pyarrow")
     mem_name = os.path.join(save_dir, "membership")
     dd.to_parquet(membership, mem_name, compression="gzip", compute=True, engine="pyarrow")
+    logger.warning("%s Finished orient scaffolds", time.ctime())
     return mem_name, res_name
