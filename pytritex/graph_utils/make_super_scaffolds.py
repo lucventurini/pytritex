There is a bug in the initial membership calculation that results in multiple presences for the same scaffold. Starting to refactor the code to track and squash the bug.

pytritex/graph_utils/make_super_scaffolds.py
@@ -11,14 +11,27 @@ from typing import Union
 logger = logging.getLogger("distributed.worker")
 
 
-def make_super_scaffolds(links: Union[str, dd.DataFrame],
-                         info: Union[str, dd.DataFrame],
-                         membership: Union[str, dd.DataFrame],
-                         save_dir: str,
-                         client: Client,
-                         excluded=None,
-                         to_parquet=True,
-                         ncores=1):
+def get_previous_groups(membership):
+    """This function will retrieve the previous groups to which scaffolds have been assigned.
+    We only consider groups where the maximum rank is 1.
+    This has the purpose of avoiding recomputing groups for nothing during the TSP stage."""
+
+    if membership is not None:
+        if isinstance(membership, str):
+            membership = dd.read_parquet(membership, infer_divisions=True)
+        else:
+            assert isinstance(membership, dd.DataFrame)
+        membership = membership.loc[
+            membership.super_size > 1, ["super", "bin", "rank", "backbone"]].compute()
+        membership["max_rank"] = membership.groupby("super")["rank"].transform("max")
+        membership = membership.loc[membership["max_rank"] <= 1].drop("max_rank", axis=1)
+    else:
+        membership = pd.DataFrame(
+            columns=["scaffold_index", "super", "bin", "rank", "backbone"]).set_index("scaffold_index")
+    return membership
+
+
+def prepare_tables(links, info, membership, excluded):
     if isinstance(links, str):
         links = dd.read_parquet(links, infer_divisions=True)
     else:
@@ -31,21 +44,7 @@ def make_super_scaffolds(links: Union[str, dd.DataFrame],
         info = dd.read_parquet(info, infer_divisions=True)
     else:
         assert isinstance(info, dd.DataFrame)
-
-    if membership is not None:
-        if isinstance(membership, str):
-            membership = dd.read_parquet(membership, infer_divisions=True)
-        else:
-            assert isinstance(membership, dd.DataFrame)
-        # cluster=results[:, 0], bin=results[:, 1], rank=results[:, 2], backbone=results[:, 3])
-        membership = membership.loc[
-            membership.super_size > 1, ["super", "bin", "rank", "backbone"]].compute()
-        membership["max_rank"] = membership.groupby("super")["rank"].transform("max")
-        membership = membership.loc[membership["max_rank"] <= 1].drop("max_rank", axis=1)
-    else:
-        membership = pd.DataFrame(
-            columns=["scaffold_index", "super", "bin", "rank", "backbone"]).set_index("scaffold_index")
-
+    membership = get_previous_groups(membership)
     info2 = info.loc[:, ["popseq_chr", "popseq_cM", "length"]].rename(
         columns={"popseq_chr": "chr", "popseq_cM": "cM"})
     assert "popseq_chr" not in info2.columns and "chr" in info2.columns
@@ -55,32 +54,48 @@ def make_super_scaffolds(links: Union[str, dd.DataFrame],
     else:
         excluded = pd.Series([], name="scaffold_index")
         excluded_scaffolds = excluded.copy()
-    input_df = info2.assign(excluded=info.index.isin(excluded_scaffolds))
-    input_df.index = input_df.index.rename("cluster")
+    cluster_info = info2.assign(excluded=info.index.isin(excluded_scaffolds))
+    cluster_info.index = cluster_info.index.rename("cluster")
     hl = links.copy().rename(columns={"scaffold_index1": "cluster1", "scaffold_index2": "cluster2"})
-    super_scaffolds = make_super(
-        hl=hl,
-        cluster_info=input_df,
-        previous_membership=membership,
-        client=client,
-        verbose=False, cores=ncores,
-        paths=True, path_max=0, known_ends=False, maxiter=100)
-    mem_copy = super_scaffolds["membership"]
-    assert mem_copy.index.name == "cluster"
-    mem_copy.index = mem_copy.index.rename("scaffold_index")
-    maxidx = super_scaffolds["super_info"]["super"].max().compute()
-    assert mem_copy.index.name == "scaffold_index"
-    iindex = info.index.values.compute()
-    bait_index = ~np.in1d(iindex, mem_copy.index.values.compute())
-    bait = iindex[bait_index]
+    return links, info, membership, excluded_scaffolds, cluster_info, hl
+
+
+def add_missing_scaffolds(info, membership, maxidx, excluded_scaffolds, client):
+    # _to_concatenate <- info[!m$scaffold, on = "scaffold"]
+    # _to_concatenate <- _to_concatenate[,.(
+    # scaffold, bin=1, rank=0, backbone=T, chr=popseq_chr, cM=popseq_cM, length=length,
+    # excluded=scaffold % in % excluded_scaffolds, super = paste0(
+    #     prefix, "_", maxidx + 1:.N))])
+    # rbind(m, [
+    #   ->m
+
+    indices = membership.index.values.compute()
+    if indices.shape[0] > len(set(indices)):
+        logger.error("Something went wrong in the calculation, we have duplicated indices.")
+        import sys
+        sys.exit(1)
+
+    assert membership.index.name == "scaffold_index"
+    # TODO: this part of the code is probably causing the double counting
+    info_index = info.index.values.compute()
+    bait_index = ~np.in1d(info_index, indices)
+    bait = info_index[bait_index]
+    # Now of course the sum of bait and indices should be equal to info_index.
+    if bait.shape[0] + indices.shape[0] != info_index.shape[0]:
+        if bait.shape[0] + indices.shape[0] > info_index.shape[0]:
+            logger.error("Something went wrong in getting the bait, we have duplicated indices.")
+        else:
+            logger.error("Something went wrong in getting the bait, we have missing indices.")
+        import sys
+        sys.exit(1)
 
     _to_concatenate = info.loc[bait, ["popseq_chr", "popseq_cM", "length"]].rename(
         columns={"popseq_chr": "chr", "popseq_cM": "cM"})
+    assert _to_concatenate.shape[0].compute() == bait.shape[0]
 
-    _to_concatenate = client.scatter(_to_concatenate)
-    chunks = delayed(lambda df: df.map_partitions(len))(_to_concatenate)
-    chunks = tuple(client.compute(chunks).result().compute().values.tolist())
-    _to_concatenate = client.gather(_to_concatenate)
+    chunks = client.compute(delayed(
+        lambda df: df.map_partitions(len))(client.scatter(_to_concatenate))).result()
+    chunks = tuple(chunks.compute().values.tolist())
 
     sup_column = da.from_array(
         maxidx + pd.Series(range(1, info.loc[bait].shape[0].compute() + 1)), chunks=chunks)
@@ -90,11 +105,18 @@ def make_super_scaffolds(links: Union[str, dd.DataFrame],
         bin=1, rank=0, backbone=True,
         excluded=excl_column,
         super=sup_column)
-    _to_concatenate = client.scatter(_to_concatenate)
-    mem_copy = client.scatter(mem_copy)
-    func = delayed(dd.concat)([mem_copy, _to_concatenate])
-    mem_copy = client.compute(func).result()
-    mem_sup_group = mem_copy.groupby("super")
+    func = delayed(dd.concat)([client.scatter(membership),
+                               client.scatter(_to_concatenate)])
+    membership = client.compute(func).result()
+
+    return membership
+
+
+def add_statistics(membership, client):
+    # m[,.(n=.N, nbin=max(bin), max_rank = max(rank), length = sum(length)), key = super]->res
+    # res[,.(super, super_size=n, super_nbin=nbin)][m, on = "super"]->m
+
+    mem_sup_group = membership.groupby("super")
     res_size = mem_sup_group.size().to_frame("n")
     res_cols = mem_sup_group.agg(
         {"bin": "max", "rank": "max", "length": "sum"})
@@ -103,18 +125,56 @@ def make_super_scaffolds(links: Union[str, dd.DataFrame],
     left = res.loc[:, ["n", "nbin"]].rename(
         columns={"n": "super_size", "nbin": "super_nbin"})
     left = client.scatter(left)
-    mem_copy = client.scatter(mem_copy)
-    func = delayed(dd.merge)(left, mem_copy, left_index=True, right_on="super",
+    membership = client.scatter(membership)
+    func = delayed(dd.merge)(left, membership, left_index=True, right_on="super",
                              how="right")
-    mem_copy = client.compute(func).result()
-    mem_copy_iname = mem_copy.index.name
-    mem_copy = mem_copy.reset_index(drop=False).set_index(mem_copy_iname)
+    membership = client.compute(func).result()
+    mem_copy_iname = membership.index.name
+    membership = membership.reset_index(drop=False).set_index(mem_copy_iname)
+    return membership, res
+
+
+def make_super_scaffolds(links: Union[str, dd.DataFrame],
+                         info: Union[str, dd.DataFrame],
+                         membership: Union[str, dd.DataFrame],
+                         save_dir: str,
+                         client: Client,
+                         excluded=None,
+                         to_parquet=True,
+                         ncores=1):
+
+    # Links, info are as they were.
+    # "Membership", if given, provides the pre-calculated groupings.
+    # Excluded scaffolds is the list of scaffolds to ignore.
+    # cluster_info is a dataframe of the form:
+    # chr, cM, length, excluded
+    # index is cluster == scaffold_index
+    # HL: is the list of links to consider
+
+    links, info, membership, excluded_scaffolds, cluster_info, hl = prepare_tables(
+        links, info, excluded=excluded, membership=membership)
+
+    super_scaffolds = make_super(
+        hl=hl,
+        cluster_info=cluster_info,
+        previous_membership=membership,
+        client=client,
+        verbose=False, cores=ncores,
+        paths=True, path_max=0, known_ends=False, maxiter=100)
+
+    # Take the membership table. Extract the scaffolds IDs which are *not* present in the table.
+    membership = super_scaffolds["membership"]
+    assert membership.index.name == "cluster"
+    membership.index = membership.index.rename("scaffold_index")
+    maxidx = super_scaffolds["super_info"]["super"].max().compute()
+    membership = add_missing_scaffolds(info, membership, maxidx, excluded_scaffolds, client)
+    membership, res = add_statistics(membership, client)
     # mem_copy = dd.from_pandas(mem_copy, chunksize=1000)
     if to_parquet is True:
-        dd.to_parquet(mem_copy, os.path.join(save_dir, "membership"))
+        dd.to_parquet(membership, os.path.join(save_dir, "membership"))
         # res = dd.from_pandas(res, chunksize=1000)
         dd.to_parquet(res, os.path.join(save_dir, "result"))
         return {"membership": os.path.join(save_dir, "membership"),
                 "info": os.path.join(save_dir, "result")}
     else:
-        return {"membership": mem_copy, "info": res}
+        return {"membership": membership, "info": res}

pytritex/scaffold_10x/orient_scaffolds.py
@@ -66,6 +66,7 @@ def orient_scaffolds(info: str, res: str,
     info = dd.read_parquet(info, infer_divisions=True)
     res = dd.read_parquet(res, infer_divisions=True)
 
+    membership = membership.drop_duplicates()
     # #  m[super_nbin > 1, .(scaffold1=scaffold, bin1=bin, super1=super)][link_pos, on="scaffold1", nomatch=0]->a
     logger.warning("%s Getting scaffolds in super-scaffolds", time.ctime())
     m_greater_one = membership.query("super_nbin > 1")
@@ -127,7 +128,7 @@ def orient_scaffolds(info: str, res: str,
     func = delayed(dd.merge)(client.scatter(membership),
                              client.scatter(final_association[["orientation"]]),
                              left_index=True, right_index=True, how="left")
-    membership = client.compute(func).result()
+    membership = client.compute(func).result().drop_duplicates()
     logger.warning("%s Merged the orientation variable back into membership", time.ctime())
 
     # Now assign an "orientation" value to each (1 if + or unknown, -1 for -)
@@ -152,7 +153,13 @@ def orient_scaffolds(info: str, res: str,
     # This position should be determined by bin and rank.
 
     logger.warning("%s Calculating the positions within super-scaffolds", time.ctime())
-    super_pos = membership.groupby("super").apply(super_position, meta=np.int).compute()
+    excluded = membership[(membership["excluded"] == True) | (membership.super_size == 1)]
+    excluded["super_pos"] = 1
+    # chunks = tuple(excluded.map_partitions(len).compute().values.tolist())
+    # excluded["super_pos"] = np.nan
+    non_excluded = membership[(membership["excluded"] == False) & (membership.super_size > 1)]
+    grouped = non_excluded.groupby("super")
+    super_pos = grouped.apply(super_position, meta=np.int).compute()
     super_pos = np.concatenate(super_pos.values)
     chunks = tuple(membership.map_partitions(len).compute().values.tolist())
     super_pos = da.from_array(super_pos, chunks=chunks)