689257ea4642c880cf469df0341ba27d9eb9bd52
Luca Venturini <lucventurini@gmail.com>
2020-07-04 10:23:29 +0100

    Codified the make_agp routine

--- pytritex/scaffold_10x/make_agp.py
+++ pytritex/scaffold_10x/make_agp.py
@@ -1,35 +1,45 @@
 import pandas as pd
 import numpy as np
+import dask.dataframe as dd
 
 
-def make_agp(membership: pd.DataFrame, gap_size=100):
+def make_agp(membership: dd.DataFrame, info: dd.DataFrame, gap_size=100):
 
-    pre_agp = membership.loc[:, ["scaffold", "length", "super", "bin", "orientation"]]
-    pre_agp = pre_agp.sort_values(["super", "bin", "length"], ascending=[True, True, False])
-    pre_agp.loc[:, "index"] = 2 * pd.Series(range(1, pre_agp.shape[0] + 1)) - 1
-    pre_agp.loc[:, "gap"] = False
-    pre_agp = pd.concat([
-        pre_agp,
-            pd.DataFrame({"scaffold": "gap", "gap": True, "super": pre_agp["super"], "bin": np.nan,
-                      "length": gap_size, "orientation": np.nan, "index": pre_agp["index"] + 1})
-    ])
-    # Group and sort by iter. Get all rows except the last one (as it is an extra gap).
-    # Also store the number of scaffolds in the super-scaffold bin (in the "n" column)
-    agp_grouped = pre_agp.sort_values("index").groupby("super")
-    agp = pd.concat([agp_grouped.get_group(index).head(n)
-                    for index, n in (agp_grouped.size() - 1).iteritems()])
-    # Calculate point of insertion of each scaffold/gap within the super-scaffold
-    agp.loc[:, "super_start"] = agp.groupby("super")["length"].shift().cumsum().fillna(0) + 1
-    agp.loc[:, "super_end"] = agp.groupby("super")["length"].cumsum()
-    #   z[, .(scaffold=scaffold, bed_start=0, bed_end=length,
-    #   name=scaffold, score=1, strand=ifelse(is.na(orientation) | orientation == 1, "+", "-"), super=super)]->agp_bed
-    agp_bed = pre_agp.loc[:, ["scaffold", "length", "orientation", "super"]]
-    agp_bed.loc[:, "bed_start"] = 0
-    agp_bed.loc[:, "bed_end"] = agp_bed.loc[:, "length"]
-    agp_bed.loc[:, "name"] = agp_bed.loc[:, "scaffold"]
-    agp_bed.loc[:, "score"] = 1
-    agp_bed.loc[(agp_bed["orientation"].isna()) | (agp_bed["orientation"] == 1), "strand"] = "+"
-    agp_bed.loc[agp_bed["strand"].isna(), "strand"] = "-"
-    del agp_bed["orientation"]
-    agp_bed = agp_bed[["scaffold", "bed_start", "bed_end", "name", "score", "strand", "super"]]
-    return {"agp": pre_agp, "agp_bed": agp_bed}
+    if "scaffold_index" not in membership.columns:
+        assert "scaffold_index" == membership.index.name
+        data = membership[["length", "super", "bin", "orientation", "rank"]].reset_index(drop=False).compute()
+    else:
+        assert membership.index.name is None
+        data = membership[
+            ["scaffold_index", "length", "super", "bin", "orientation", "rank"]
+        ].reset_index(drop=True).compute()
+    data = data.sort_values(["super", "bin", "length"], ascending=[True, True, False])
+    data.loc[:, "index"] = pd.Series(np.arange(1, data.shape[0] + 1) * 2 - 1, index=data.index)
+    data.loc[:, "gap"] = False
+    # Now create the gap rows
+    gaps = pd.DataFrame().assign(scaffold_index=np.nan,
+                                 super=data["super"].values,
+                                 bin=np.nan,
+                                 length=gap_size,
+                                 orientation=np.nan,
+                                 index=data["index"].values + 1)
+    # Probably "gap" is a reserved keyword
+    gaps.loc[:, "gap"] = True
+    data = pd.concat([data, gaps]).sort_values("index")
+    # Remove the last row as it is a dangling gap.
+    data = data[data.groupby("super").cumcount(ascending=False) > 0]
+    # Assign the super_start position
+    data.loc[:, "super_start"] = data.groupby("super")["length"].shift(1).transform("cumsum").fillna(0) + 1
+    data.loc[:, "super_end"] = data.groupby("super")["length"].transform("cumsum")
+    scaffolds = info[["scaffold"]].compute().reset_index(drop=False)
+    scaffolds = scaffolds.append({"scaffold_index": np.nan, "scaffold": "gap"}, ignore_index=True)
+    data = data.merge(scaffolds, how="left", on="scaffold_index")
+    # Now create the BED file
+    agp_bed = pd.DataFrame().assign(
+        scaffold_index=data["scaffold_index"].values,
+        scaffold=data["scaffold"].values, bed_start=0,
+        bed_end=data["length"].values, name=data["scaffold"].values,
+        score=1, strand=data["orientation"].map({1: "+", -1: "-", np.nan: "+"}).values,
+        super=data["super"].values)
+
+    return {"agp": data, "agp_bed": agp_bed}
