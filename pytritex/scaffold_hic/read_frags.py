0edbbd276ee595c3f4367db478994c6c81617180
Luca Venturini <lucventurini@gmail.com>
2020-09-23 18:01:19 +0100

    Fixed the reading of frag data. Ready for actually creating the pseudomolecules with HiC?

--- pytritex/scaffold_hic/read_frags.py
+++ pytritex/scaffold_hic/read_frags.py
@@ -52,20 +52,19 @@ def read_fragdata(fai, fragfile, map_10x, savedir=None):
     if isinstance(fai, str):
         fai = dd.read_parquet(fai, infer_divisions=True)
     
-    left = fai.query("derived_from_split == False")[
+    left = fai.query("derived_from_split == False")[:][
         ["scaffold", "orig_scaffold_index", "to_use"]].reset_index(drop=True).rename(
-        columns={"scaffold": "orig_scaffold"}).set_index("orig_scaffold")
+        columns={"scaffold": "orig_scaffold"}).set_index("orig_scaffold").persist()
 
     frags = dd.read_csv(fragfile, names=["orig_scaffold", "start", "end"], delimiter="\t", header=None,
-                        dtype={"orig_scaffold": str, "start": np.int32, "end": np.int32})
+                        dtype={"orig_scaffold": str, "start": int, "end": int})
     frags["length"] = frags["end"] - frags["start"]
-    frags["start"] += 1    
+    frags["start"] += 1
+    frags = frags.set_index("orig_scaffold")
+    frags = frags.persist()
     
     frags = dd.merge(left, frags, on="orig_scaffold").reset_index(drop=True).set_index("orig_scaffold_index")
-    if savedir is not None:
-        path = dd.to_parquet(frags, os.path.join(savedir, "raw_frags"),
-                             compression="gzip", compute=True, engine="pyarrow")
-    
+    frags = frags.persist()
     # Select scaffolds that have not been broken up, we'll use them as-is
     frags_to_keep = frags.query("to_use == True")[:].drop("to_use", axis=1)
     frags_to_keep.index = frags_to_keep.index.rename("scaffold_index")
@@ -77,45 +76,40 @@ def read_fragdata(fai, fragfile, map_10x, savedir=None):
     left = left[["orig_scaffold_index", "orig_start"]].reset_index(drop=False).set_index("orig_scaffold_index")
     left["start"] = left["orig_start"]
     frags_to_roll = rolling_join(left, frags_to_roll, on="orig_scaffold_index",
-                                 by="start").reset_index(drop=False).set_index("scaffold_index")
+                                 by="start").set_index("scaffold_index")
     frags_to_roll["end"] -= frags_to_roll["orig_start"] + 1
     frags_to_roll["start"] -= frags_to_roll["orig_start"] + 1
     frags_to_roll = frags_to_roll.drop(["orig_start", "orig_scaffold_index"], axis=1)
 
     # Now concatenate for the final frag file.
-    frags = dd.concat([frags_to_keep, frags_to_roll]).astype({"start": np.int32, "end": np.int32})
+    frags = dd.concat([frags_to_keep, frags_to_roll], axis="index").astype({"start": np.int32, "end": np.int32}).persist()
+    # sort by scaffold_index
+    frags = frags.reset_index(drop=False).set_index("scaffold_index")
     agp = map_10x["agp"]
     if isinstance(agp, str):
         agp = dd.read_parquet(agp)
     
-    left = agp.query("gap == False")[["super", "orientation", "super_start", "super_end"]]
-    fragbed = dd.merge(left, frags, on="scaffold_index")
+    left = agp.query("gap == False")[["super", "orientation", "super_start", "super_end", "scaffold_index"]]
+    left = left.set_index("scaffold_index")
+    fragbed = dd.merge(left, frags, on="scaffold_index").persist()
     # map_10x$agp[gap == F, .(super, orientation, super_start, super_end, scaffold)][fragbed, on="scaffold"]->fragbed
-    bait = (fragbed["orientation"] == 1)
-    fragbed.loc[bait, ["start"]] = fragbed.loc[bait]["super_start"] - 1 + fragbed.loc[bait]["start"]
-    fragbed.loc[bait, ["end"]] = fragbed.loc[bait]["super_start"] - 1 + fragbed.loc[bait]["end"]
-    bait = (fragbed["orientation"] == -1)
-    fragbed.loc[bait, ["start"]] = fragbed.loc[bait]["super_end"] + 1 - fragbed.loc[bait]["end"]
-    fragbed.loc[bait, ["end"]] = fragbed.loc[bait]["super_end"] + 1 - fragbed.loc[bait]["start"]
-    fragbed = fragbed.drop(["orientation", "super_start", "super_end", "scaffold"], axis=1)
-    fragbed = fragbed.rename(columns={"super": "orig_scaffold"})
+    fragbed["frag_start"] = fragbed["start"].mask(fragbed["orientation"] == 1, fragbed["super_start"] - 1 + fragbed["start"])
+    fragbed["frag_end"] = fragbed["end"].mask(fragbed["orientation"] == 1, fragbed["super_start"] - 1 + fragbed["end"])    
+    fragbed["frag_start"] = fragbed["start"].mask(fragbed["orientation"] == -1, fragbed["super_end"] + 1 - fragbed["end"])
+    fragbed["frag_end"] = fragbed["end"].mask(fragbed["orientation"] == -1, fragbed["super_end"] + 1 - fragbed["start"])
+    fragbed = fragbed.drop(["start", "end", "super_start", "super_end"], axis=1, errors="ignore")
+    fragbed = fragbed.rename(columns={"frag_start": "start", "frag_end": "end"})
+    # fragbed = fragbed.rename(columns={"super": "orig_scaffold_index"})
+    fragbed = fragbed.reset_index(drop=True).set_index("super").repartition(
+        npartitions=int(fragbed.shape[0].compute() // 10 ** 6)).persist()
     # TODO: is there "info" in map_10x?
-    left = map_10x["info"][["orig_start", "orig_scaffold_index"]].reset_index(drop=False).set_index(
-        "orig_scaffold_index")
-    left["start"] = left["orig_start"]
-    # assembly_10x$info[,
-    # .(scaffold, start=orig_start, orig_start, orig_scaffold)][fragbed, on=c("orig_scaffold", "start"), roll=T]->fragbed
-    fragbed = rolling_join(left, fragbed, on="orig_scaffold_index", by="start")
-    #   fragbed[, start := start - orig_start + 1]
-    #   fragbed[, end := end - orig_start + 1]
-    #   fragbed[, orig_start := NULL]
-    #   fragbed[, orig_scaffold := NULL]
-    #
-    #   fragbed[, .(nfrag = .N), keyby=scaffold][assembly_10x$info, on="scaffold"][is.na(nfrag), nfrag := 0]->z
-    fragbed = fragbed.eval("""start = start - orig_start + 1
-    end = end - orig_start + 1""").drop(["orig_start"]).reset_index(drop=True).set_index("scaffold_index")
-    info = fragbed.groupby("scaffold_index").size().to_frame("nfrag")
-    info = info.merge(map_10x["info"], on="scaffold_index", how="right")
+    info = map_10x["fai"]
+    if isinstance(info, str):
+        info = dd.read_parquet(info, infer_divisions=True)
+
+    fragbed.index = fragbed.index.rename("scaffold_index")
+    nfrags = fragbed.groupby("scaffold_index").size().to_frame("nfrag")
+    info = nfrags.merge(info, on="scaffold_index", how="right")
     info["nfrag"] = info["nfrag"].fillna(0)
     if savedir is not None:
         fragbed_name = os.path.join(savedir, "fragbed")
